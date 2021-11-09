4443ff4304763233a8d69420ec18db7a5245a077
Scenaristeur <scenaristeur@gmail.com>
2021-11-09 19:22:52 +0100

    wiki 404

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <!-- <Login /> -->
     <!-- <ResourcesList /> -->
     <!-- <Game /> -->
-    <small><i>0.0.11 - wiki</i></small>
+    <small><i>0.0.12 - wiki & 404</i></small>
   </div>
 </template>
 
