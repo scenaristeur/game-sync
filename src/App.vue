bf91b8fafdd6a574b053961588dd800638a9490f
Scenaristeur <scenaristeur@gmail.com>
2021-11-11 11:20:55 +0100

    tabs

--- src/App.vue
+++ src/App.vue
@@ -44,12 +44,13 @@ color: #42b983;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
+  text-align: center;
 }
 
 #nav a {
--- src/components/wiki/WikiEntry.vue
+++ src/components/wiki/WikiEntry.vue
@@ -1,13 +1,60 @@
 <template>
+  <div>
+    <div v-if="wikiEntry != null">
 
-    <ul v-if="wikiEntry != null">
-      <li v-for="(t, id) in wikiEntry.things" :key="id">
-        {{t.name}}
-      </li>
-    </ul>
-    <!-- <small>
-      {{JSON.stringify(wikiEntry)}}
-    </small> -->
+      <div>
+        <b-card no-body>
+          <b-tabs pills card vertical>
+            <!-- -->
+            <b-tab v-for="(t, id) in wikiEntry.things" :key="id"
+            :title="t.name">
+
+            <b-card-text>
+              {{t.name}}
+              {{t.text}}
+
+              <blockquote contenteditable="true" v-html="t.text">
+                <p>Edit this content to add your own quote</p>
+              </blockquote>
+
+              <cite contenteditable="true">-- Write wikidata or semapps tags here, coma separated</cite>
+
+            </b-card-text>
+
+          </b-tab>
+
+          <b-tab title="other thing" ><b-card-text>other entry in this wiki page</b-card-text></b-tab>
+          <b-tab title="Tab 2"><b-card-text>Tab contents 2</b-card-text></b-tab>
+          <b-tab title="Tab 3"><b-card-text>Tab contents 3</b-card-text></b-tab>
+
+
+        </b-tabs>
+      </b-card>
+    </div>
+
+    <!-- <div v-for="(t, id) in wikiEntry.things" :key="id">
+    <b-button v-b-toggle :href="'#_'+id" @click.prevent>{{t.name}}</b-button>
+
+
+    <b-collapse :id="'#_'+id">
+    <b-card title="Collapsible card">
+    Hello world!Hello world! {{id}}
+  </b-card>
+</b-collapse>
+</div> -->
+
+</div>
+
+<!-- <ul v-if="wikiEntry != null">
+<li v-for="(t, id) in wikiEntry.things" :key="id">
+{{t.name}}
+
+</li>
+</ul> -->
+</div>
+<!-- <small>
+{{JSON.stringify(wikiEntry)}}
+</small> -->
 
 </template>
 
@@ -40,5 +87,34 @@ export default {
 </script>
 
 <style>
+.output {
+    font: 1rem 'Fira Sans', sans-serif;
+}
+
+blockquote {
+    background: #eee;
+    border-radius: 5px;
+    margin: 16px 0;
+}
+
+blockquote p {
+    padding: 15px;
+}
+
+cite {
+    margin: 16px 32px;
+}
+
+blockquote p::before {
+    content: '\201C';
+}
+
+blockquote p::after {
+    content: '\201D';
+}
+
+[contenteditable='true'] {
+    caret-color: red;
+}
 
 </style>
